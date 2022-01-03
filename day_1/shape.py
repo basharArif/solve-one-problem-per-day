903e58324e009bbafe9ec0e1a4f92b40b988eaec
nittanando <sarkar.anando2995@gmail.com>
2022-01-03 21:18:08 +0600

    day-2

--- day_1/shape.py
+++ day_1/shape.py
@@ -3,18 +3,29 @@ userinput = int(input("enter value: "))
 # printing shapes in python
 
 # no-1
+
+for i in range (userinput):
+    for j in range(userinput):
+        print("1 ",end="")
+
+    print("")
+
+# no-2
+
 for i in range(userinput):
     for j in range(i+1):
         print("*",end=" ")
     print(" ")
 
-# no-2
+# no-3
+
 for i in range(userinput):
     for j in range(userinput-i):
         print("*",end=" ")
     print(" ")
 
-# no-3
+# no-4
+
 k = (userinput*2)-2
 for i in range(userinput):
     for j in range(k):
@@ -24,3 +35,24 @@ for i in range(userinput):
     for a in range(i+1):
         print("*", end=" ")
     print("\r")
+
+# no-5
+
+for i in range(1,5):
+    for j in range(userinput):
+        print(i,end=" ")
+    print("")
+
+# no-6
+
+for i in range(1,5):
+    for j in range(userinput):
+        print(userinput-i,end=" ")
+    print("")
+
+# no-7
+
+for i in range(1,5):
+    for j in range(1,userinput):
+        print(j,end=" ")
+    print("")
