fb8f95503e9439ad26903f4542cefff8dc70d57c
Nahid570 <farajinahid2@gmail.com>
2022-01-10 22:25:04 +0600

    c

---
+++ c.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    int num1;
+    int num2;
+
+    printf("Please enter two integer number\n");
+
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+
+    printf("Add of two number is: %d", num1 + num2);
+
+    return 0;
+}
